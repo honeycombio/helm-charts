c7449253187bad0e6d0c48ef232d15f0b357c925
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2022-10-19 12:31:51 -0600

    revert changes to install_tools.sh

--- .circleci/install_tools.sh
+++ .circleci/install_tools.sh
@@ -16,6 +16,7 @@ rm -f "helm-v$HELM_VERSION-linux-amd64.tar.gz"
 helm repo add stable https://charts.helm.sh/stable
 helm repo add bitnami https://charts.bitnami.com/bitnami
 
+
 echo "Installing chart-releaser..."
 curl -LO "https://github.com/helm/chart-releaser/releases/download/v${CHART_RELEASER_VERSION}/chart-releaser_${CHART_RELEASER_VERSION}_linux_amd64.tar.gz"
 sudo mkdir -p "/usr/local/chart-releaser-v$CHART_RELEASER_VERSION"
