c1ea5f571d65e3b1d7909e26354a3fb8da16ac78
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2023-05-12 15:31:45 -0600

    Remove secure tenancy chart

--- .circleci/install_tools.sh
+++ .circleci/install_tools.sh
@@ -13,9 +13,6 @@ sudo ln -s "/usr/local/helm-v$HELM_VERSION/linux-amd64/helm" /usr/local/bin/helm
 rm -f "helm-v$HELM_VERSION-linux-amd64.tar.gz"
 
 # setup chart dependency repositories here
-helm repo add stable https://charts.helm.sh/stable
-helm repo add bitnami https://charts.bitnami.com/bitnami
-
 
 echo "Installing chart-releaser..."
 curl -LO "https://github.com/helm/chart-releaser/releases/download/v${CHART_RELEASER_VERSION}/chart-releaser_${CHART_RELEASER_VERSION}_linux_amd64.tar.gz"
