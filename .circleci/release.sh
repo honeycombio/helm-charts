8df1b728040f47bfaee101e15d7d116e8cf832f3
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2023-12-01 09:15:06 -0700

    maint: Release on every commit (#309)
    ## Which problem is this PR solving?
    
    Currently to release a chart we must create a prep PR and then manually
    tag. Based on the infrequency of releases and the scope of what is being
    released, we now consider this toil.
    
    This PR updates the repo's release process to be in line with other
    popular charts like opentelemetry, jaeger, and prometheus. With these
    changes a chart will be released on every commit to main.
    
    ## Short description of the changes
    
    - Updates the release to identify and release charts that have changed
    in the latest commit
    - Since we use circleci we coudn't use the chart-releaser-action
    directly, so instead we use their bash script.
    - Updated docs
    
    ## How to verify that this has the expected result

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# https://github.com/helm/chart-releaser-action/blob/main/cr.sh
+# Copyright The Helm Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -9,66 +24,94 @@ set -o pipefail
 : "${GIT_REPOSITORY_OWNER:?Environment variable GIT_REPOSITORY_OWNER must be set}"
 : "${GIT_REPOSITORY_NAME:?Environment variable GIT_REPOSITORY_NAME must be set}"
 
-readonly REPO_ROOT="${REPO_ROOT:-$(git rev-parse --show-toplevel)}"
-
 main() {
-  pushd "$REPO_ROOT" >/dev/null
-  echo "Fetching tags..."
-  git fetch --tags
+  local charts_dir=charts
+  
+  local repo_root
+  repo_root=$(git rev-parse --show-toplevel)
+  pushd "$repo_root" >/dev/null
 
+  echo 'Looking up latest tag...'
   local latest_tag
-  latest_tag=$(git describe --tags --abbrev=0)
-  echo "Latest Tag: $latest_tag"
+  latest_tag=$(lookup_latest_tag)
 
-  if [[ ! "$latest_tag" =~ ^v[0-9].+-.* ]]; then
-    echo "Latest Tag is not a 'v' Release tag"
-    exit 2
+  echo "Discovering changed charts since '$latest_tag'..."
+  local changed_charts=()
+  readarray -t changed_charts <<<"$(lookup_changed_charts "$latest_tag")"
+
+  if [[ -n "${changed_charts[*]}" ]]; then
+    rm -rf .cr-release-packages
+    mkdir -p .cr-release-packages
+
+    rm -rf .cr-index
+    mkdir -p .cr-index
+
+    for chart in "${changed_charts[@]}"; do
+      if [[ -d "$chart" ]]; then
+        package_chart "$chart"
+      else
+        echo "Nothing to do. No chart changes detected."
+      fi
+    done
+
+    release_charts
+    update_index
+  else
+    echo "Nothing to do. No chart changes detected."
   fi
-
-  local chart_name
-  # shellcheck disable=SC2001
-  chart_name=$(echo "$latest_tag" | sed "s/^v.[^\-]*-\(.*\)/\1/")
-
-  echo "Chart Name: $chart_name"
-
-  if [[ ! -d "$REPO_ROOT/charts/$chart_name" ]]; then
-    echo "Chart $chart_name not found"
-    exit 2
-  fi
-
-  rm -rf .deploy
-  mkdir -p .deploy
-
-  echo "Packaging chart $chart_name..."
-  package_chart "charts/$chart_name"
-
-  echo "Releasing charts..."
-  release_charts
-  sleep 5
-
-  echo "Updating Helm repo index..."
-  update_index
 
   popd >/dev/null
+}
 
+lookup_latest_tag() {
+  git fetch --tags >/dev/null 2>&1
+
+  if ! git describe --tags --abbrev=0 HEAD~ 2>/dev/null; then
+    git rev-list --max-parents=0 --first-parent HEAD
+  fi
+}
+
+filter_charts() {
+  while read -r chart; do
+    [[ ! -d "$chart" ]] && continue
+    local file="$chart/Chart.yaml"
+    if [[ -f "$file" ]]; then
+      echo "$chart"
+    else
+      echo "WARNING: $file is missing, assuming that '$chart' is not a Helm chart. Skipping." 1>&2
+    fi
+  done
+}
+
+lookup_changed_charts() {
+  local commit="$1"
+
+  local changed_files
+  changed_files=$(git diff --find-renames --name-only "$commit" -- "$charts_dir")
+
+  local depth=$(($(tr "/" "\n" <<<"$charts_dir" | sed '/^\(\.\)*$/d' | wc -l) + 1))
+  local fields="1-${depth}"
+
+  cut -d '/' -f "$fields" <<<"$changed_files" | uniq | filter_charts
 }
 
 package_chart() {
   local chart="$1"
-  cr package "$chart"
+  echo "Packaging chart '$chart'..."
+  cr package "$chart" --package-path .cr-release-packages
 }
 
 release_charts() {
+  echo 'Releasing charts...'
   cr upload --git-repo "$GIT_REPOSITORY_NAME" --owner "$GIT_REPOSITORY_OWNER" --token "$GITHUB_TOKEN"
 }
 
 update_index() {
+  echo 'Updating charts repo index...'
   git config user.name "$GIT_USERNAME"
   git config --global --unset url.ssh://git@github.com.insteadof
   git remote add origin-https "https://github.com/$GIT_REPOSITORY_OWNER/$GIT_REPOSITORY_NAME.git"
   git fetch --all
-
-  mkdir .cr-index
   cr index --git-repo "$GIT_REPOSITORY_NAME" --owner "$GIT_REPOSITORY_OWNER" --token "$GITHUB_TOKEN" --remote "origin-https" --push
 }
 
