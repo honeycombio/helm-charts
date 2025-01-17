c36c59d899a6ab1540177bf466aa56e2b4d882aa
Tyler Helmuth <12352919+TylerHelmuth@users.noreply.github.com>
2025-01-17 09:15:48 -0700

    maint: fix release

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -101,6 +101,7 @@ add_dependencies() {
   echo "Adding chart dependencies"
   helm repo add honeycomb https://honeycombio.github.io/helm-charts
   helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
+  helm repo add bindplane https://observiq.github.io/bindplane-op-helm
 }
 
 package_chart() {
